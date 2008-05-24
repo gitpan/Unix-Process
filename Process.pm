2b3638a92de329ec6641f1adaa3801c7e721df7f
Paul Miller <jettero@cpan.org>
2008-05-24 14:08:48 +0000

    Import of JETTERO/Unix-Process-1.002000 from CPAN.
    gitpan-cpan-distribution: Unix-Process
    gitpan-cpan-version:      1.002000
    gitpan-cpan-path:         JETTERO/Unix-Process-1.002000.tar.gz
    gitpan-cpan-author:       JETTERO
    gitpan-cpan-maturity:     released

--- Process.pm
+++ Process.pm
@@ -7,12 +7,14 @@ use strict;
 use warnings;
 use AutoLoader;
 use Carp;
+use IPC::System::Simple qw(capture);
 
-our $VERSION = "1.1.1";
+use version;
+our $VERSION    = version->new("1.2.0")->numify;
+our $PS_PROGRAM = $ENV{PS_PATH} || '/bin/ps';
+our $AUTOLOAD;
 
-use vars qw($AUTOLOAD);
-
-return 1;
+1;
 
 sub AUTOLOAD {
     my $this = shift;
@@ -21,15 +23,11 @@ sub AUTOLOAD {
 
     $sub = $1 if $sub =~ m/::(\w+)$/;
 
-    my $v = `/bin/ps -o $sub -p $pid 2>&1`;
+    my $result = eval { capture($PS_PROGRAM, '-o', $sub, '-p', $pid) };
+    croak $@ if $@;
 
-    croak $1 if $v =~ m/error:(.+?)[\r\n]/ms;
-
-    if($v =~ m/[\r\n](.+?)[\r\n]/ms) {
-        return $1;
-    }
-
-    return "";
+    return $1 if $result =~ m/[\r\n](.+?)[\r\n]/ms;
+    return;
 }
 
 __END__
@@ -48,24 +46,30 @@ __END__
 
 =head1 DESCRIPTION
 
-    All fields from the ps command can be fetched by calling a
-    function of their name (see SYNOPSIS).  If the pid is not
-    given as an argument to the function, $$ (cur pid) is assumed.
+All fields from the ps command can be fetched by calling a function of their
+name (see SYNOPSIS).  If the pid is not given as an argument to the function,
+$$ (cur pid) is assumed.
 
-    BTW, this module is really just a giant AUTOLOAD to interact
-    with the /bin/ps command.  I suppose I could be talked into
-    doing something real with it some day.
+BTW, this module is really just a giant AUTOLOAD to interact with the /bin/ps
+command.  I suppose I could be talked into doing something real with it some
+day.
+
+You can manually set the C<$Unix::Process::PS_PROGRAM = "/opt/bin/ps"> by hand,
+or you can set C<$ENV{PS_PATH} = "/usr/local/bin/ps">, but you must somehow 
+instruct Unix::Process on the location of ps.  Otherwise, it will guess
+C<"/bin/ps">.
 
 =head1 AUTHOR
 
-Jettero Heller <japh@voltar-confed.org>
+Paul Miller <paul@cpan.org>
 
-   Jet is using this software in his own projects...
-   If you find bugs, please please please let him know. :)
+I am using this software in my own projects...  If you find bugs, please
+please please let me know. :) Actually, let me know if you find it handy at
+all.  Half the fun of releasing this stuff is knowing that people use it.
 
-   Actually, let him know if you find it handy at all.
-   Half the fun of releasing this stuff is knowing 
-   that people use it.
+=head1 COPYRIGHT
+
+Copyright (c) 2007 Paul Miller -- LGPL [attached]
 
 =head1 SEE ALSO
 
